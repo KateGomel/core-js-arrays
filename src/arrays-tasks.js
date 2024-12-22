af216ffd2054d8f4a27f9c37453d6939c535f8c6
KateGomel <ykatuay@gmail.com>
2024-12-22 13:25:30 +0300

    feat: add solution findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -79,8 +79,14 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((acc, currentItem) => {
+    let count = acc;
+    if (currentItem === item) {
+      count += 1;
+    }
+    return count;
+  }, 0);
 }
 
 /**
